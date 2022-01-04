079be872a386566302454d00eeca49478063b9b9
sweat <sweat.eth@gmail.com>
2022-01-04 18:35:35 -0500

    set ethers provider

--- find_toad/hardhat.config.js
+++ find_toad/hardhat.config.js
@@ -3,13 +3,14 @@
  */
  require('dotenv').config();
  require("@nomiclabs/hardhat-ethers");
+
  const { API_URL, PRIVATE_KEY } = process.env;
  module.exports = {
     solidity: "0.8.0",
-    defaultNetwork: "ropsten",
+    defaultNetwork: "ethereum",
     networks: {
        hardhat: {},
-       ropsten: {
+       ethereum: {
           url: API_URL,
           accounts: [`0x${PRIVATE_KEY}`]
        }
